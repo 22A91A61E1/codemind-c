96f706d5d2abe8349188125a36308095223ad4de
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-01-15 20:58:30 +0530

    #include<stdio.h> int main() {  float a;  scanf("%f",&a);  if(a==1)  {      printf("monday");  }  else if(a==2)  {      printf("tuesday");  }   else if(a==3)  {      printf("wednesday");  }   else if(a==4)  {      printf("thursday");  }  else if(a==5)  {      printf("friday");  }  else if(a==6)  {      printf("satuurday");  }  else  {      printf("invalid input");  } }

--- Day_name__.c
+++ Day_name__.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    float a;
-    scanf("%f",&a);
-    if (a==1)
-    {
-        printf("monday");
-    }
-    else if(a==2)
-    {
-        printf("tuesday");
-    }
-    else if(a==3)
-    {
-        printf("wednesday");
-    }
-    else if(a==4)
-    {
-        printf("thursday");
-    }
-    else if(a==5)
-    {
-        printf("friday");
-    }
-    else if(a==6)
-    {
-        printf("saturday");
-    }
-    else
-    {
-        printf("invalid input");
-    }
+ float a;
+ scanf("%f",&a);
+ if(a==1)
+ {
+     printf("monday");
+ }
+ else if(a==2)
+ {
+     printf("tuesday");
+ }
+  else if(a==3)
+ {
+     printf("wednesday");
+ }
+  else if(a==4)
+ {
+     printf("thursday");
+ }
+ else if(a==5)
+ {
+     printf("friday");
+ }
+ else if(a==6)
+ {
+     printf("satuurday");
+ }
+ else 
+ {
+     printf("invalid input");
+ }
 }
