bc2a5f8e60394040cd28bd6eb9e7b8830d32c31d
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-05-21 14:05:26 +0530

    #include<stdio.h> int main() {     int n,i,se,a[100],d=0;     float c=0,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         sum+=a[i];     }     c=(sum/n);     printf("%.2f",c); }

--- Average_of_the_array.c
+++ Average_of_the_array.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    float m=0;
+    int n,i,se,a[100],d=0;
+    float c=0,sum=0;
     scanf("%d",&n);
-    int a[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
-        
     }
     for(i=0;i<n;i++)
     {
-        m=m+a[i];
+        sum+=a[i];
     }
-    float s=m/n;
-    printf("%.2f",s);
+    c=(sum/n);
+    printf("%.2f",c);
 }
